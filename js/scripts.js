43535dbbdcc6378114e82d6647269e0cef7670a9
Ramiro Melo <ramiromelo@gmail.com>
2021-01-08 16:02:28 -0800

    Presentation

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+!(function ($) {
+  "use strict";
+
+  // Preloader
+  $(window).on("load", function () {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(100)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
+    }
+  });
+
+  // Mobile Navigation
+  if ($(".nav-menu").length) {
+    var $mobile_nav = $(".nav-menu").clone().prop({
+      class: "mobile-nav d-lg-none",
+    });
+    $("body").append($mobile_nav);
+    $(".mobile-nav").prepend(
+      '<button type="button" class="mobile-nav-close"><i class="icofont-close"></i></button>'
+    );
+    $("#header").append(
+      '<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>'
+    );
+    $("body").append('<div class="mobile-nav-overly"></div>');
+
+    $(document).on("click", ".mobile-nav-toggle", function (e) {
+      $("body").toggleClass("mobile-nav-active");
+      $(".mobile-nav-overly").toggle();
+    });
+
+    $(document).on("click", ".mobile-nav-close", function (e) {
+      $("body").removeClass("mobile-nav-active");
+      $(".mobile-nav-overly").fadeOut();
+    });
+
+    $(document).on("click", ".mobile-nav .drop-down > a", function (e) {
+      e.preventDefault();
+      $(this).next().slideToggle(300);
+      $(this).parent().toggleClass("active");
+    });
+
+    $(document).click(function (e) {
+      var container = $(".mobile-nav, .mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($("body").hasClass("mobile-nav-active")) {
+          $("body").removeClass("mobile-nav-active");
+          $(".mobile-nav-toggle i").toggleClass(
+            "icofont-navigation-menu icofont-close"
+          );
+          $(".mobile-nav-overly").fadeOut();
+        }
+      }
+    });
+  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+    $(".mobile-nav, .mobile-nav-toggle").hide();
+  }
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }
+  $(window).on("load", function () {
+    aos_init();
+  });
+})(jQuery);
